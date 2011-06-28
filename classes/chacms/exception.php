11d7b3f720739275126c8968b330e035a66fa82e
mtou <mtou@charougna.com>
2011-06-28 16:12:03 +0200

    Added ChaCMS_Exception

---
+++ classes/chacms/exception.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Exception
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/exception.php
+ * @since     2011-06-28
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Exception
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/exception.php
+ */
+class ChaCMS_Exception extends ChaCMS_Core_Exception
+{
+
+} // End class ChaCMS_Exception
