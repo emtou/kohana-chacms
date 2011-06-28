aa2136c15dd2ac3c795eaed8ee8da8ec5d8b4b34
mtou <mtou@charougna.com>
2011-06-28 14:11:41 +0200

    Created empty ChaCMS_Menu controller

---
+++ classes/chacms/core/controller/menu.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Core_Controller_Menu controller
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/controller/menu.php
+ * @since     2011-06-28
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Core_Controller_Menu controller
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/controller/menu.php
+ */
+class ChaCMS_Core_Controller_Menu extends Controller
+{
+
+} // End class ChaCMS_Core_Controller_Menu
---
+++ classes/controller/chacms/menu.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Menu controller
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/controller/chacms/menu.php
+ * @since     2011-06-28
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Menu controller
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/controller/chacms/menu.php
+ */
+class Controller_ChaCMS_Menu extends ChaCMS_Core_Controller_Menu
+{
+
+} // End class Controller_ChaCMS_Menu
