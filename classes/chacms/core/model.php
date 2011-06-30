d52f504c203781a8d9742337118bd6829704381d
mtou <mtou@charougna.com>
2011-06-30 11:08:23 +0200

    Add link manager aggregate to Core_Model

--- classes/chacms/core/model.php
+++ classes/chacms/core/model.php
@@ -33,5 +33,52 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 abstract class ChaCMS_Core_Model
 {
+  protected $_link_manager = NULL;
+
+
+  /**
+   * Creates this occurence with optional link manager aggregate
+   *
+   * @param ChaCMS_Link_Manager &$link_manager optional agregate
+   *
+   * @return null
+   */
+  public function __construct(ChaCMS_Link_Manager & $link_manager = NULL)
+  {
+    if ($link_manager instanceof ChaCMS_Link_Manager)
+    {
+      $this->link_manager($link_manager);
+    }
+  }
+
+
+  /**
+   * Gets or sets ChaCMS_Link_Manager aggregate
+   *
+   * @param ChaCMS_Link_Manager &$link_manager optional agregate (in set mode)
+   *
+   * @return ChaCMS_Link_Manager|null agregate (in get mode)
+   *
+   * @throws ChaCMS_Exception Can't find link manager aggregate: aggregate hasn\'t been set before use.
+   */
+  public function link_manager(ChaCMS_Link_Manager & $link_manager = NULL)
+  {
+    if ($link_manager == NULL)
+    {
+      // get mode
+      if ($this->_link_manager == NULL)
+      {
+        throw new ChaCMS_Exception(
+          'Can\'t find link manager aggregate: aggregate hasn\'t been set before use.'
+        );
+      }
+
+      return $this->_link_manager;
+    }
+
+    // set mode
+    $this->_link_manager = $link_manager;
+  }
+
 
 } // End class ChaCMS_Core_Model
