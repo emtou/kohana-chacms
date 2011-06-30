ee14cbbe1043c4d6f2688bac84fca5186517745b
mtou <mtou@charougna.com>
2011-06-30 09:30:07 +0200

    Extend Core_Jelly_Model from Jelly_Model

--- classes/chacms/core/model/jelly.php
+++ classes/chacms/core/model/jelly.php
@@ -31,7 +31,7 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
  * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/model/jelly.php
  */
-abstract class ChaCMS_Core_Model_Jelly
+abstract class ChaCMS_Core_Model_Jelly extends Jelly_Model
 {
 
 } // End class ChaCMS_Core_Model_Jelly
