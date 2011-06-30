7b03e1e695631a09ad087b584102be89aacdd5e2
mtou <mtou@charougna.com>
2011-06-30 09:15:54 +0200

    Add empty Linker interfaces

---
+++ classes/chacms/core/interface/linker.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Core_Interface_Linker
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/interface/linker.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Core_Interface_Linker
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/interface/linker.php
+ */
+interface ChaCMS_Core_Interface_Linker
+{
+
+} // End class ChaCMS_Core_Interface_Linker
