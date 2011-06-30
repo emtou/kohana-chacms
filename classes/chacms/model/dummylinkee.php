3742ee8d44b9887c346abde4a972a0a261b837cf
mtou <mtou@charougna.com>
2011-06-30 11:19:47 +0200

    Add DummyLinkee model

---
+++ classes/chacms/model/dummylinkee.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Model_DummyLinkee
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/model/dummylinkee.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Model_DummyLinkee
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/model/dummylinkee.php
+ */
+class ChaCMS_Model_DummyLinkee extends ChaCMS_Core_Model_DummyLinkee
+{
+
+} // End class ChaCMS_Model_DummyLinkee
