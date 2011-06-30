16fc2c0db8902df955b992bb853b365b5bfd4cad
mtou <mtou@charougna.com>
2011-06-30 09:22:27 +0200

    Add empty Model

---
+++ classes/chacms/core/model.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Core_Model
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/model.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Core_Model
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/model.php
+ */
+abstract class ChaCMS_Core_Model
+{
+
+} // End class ChaCMS_Core_Model
