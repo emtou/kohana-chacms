7069f8cb0b23fca639515de10e2c040616404126
mtou <mtou@charougna.com>
2011-07-01 11:22:31 +0200

    Remove local dependency injector container references and use global one

--- classes/chacms/core/link/manager.php
+++ classes/chacms/core/link/manager.php
@@ -31,7 +31,7 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
  * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/link/manager.php
  */
-abstract class ChaCMS_Core_Link_Manager extends ChaCMS_Injector
+abstract class ChaCMS_Core_Link_Manager
 {
   /**
    * Creates this instance
@@ -62,7 +62,7 @@ abstract class ChaCMS_Core_Link_Manager extends ChaCMS_Injector
 
     if ($hack_no_linkee)
     {
-      return $this->injector()->get('chacms.model.dummylinkee');
+      return $this->container->get('chacms.model.dummylinkee');
     }
   }
 
