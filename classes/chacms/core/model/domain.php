810c12b98de25d2a864c59c50bac6bf7916b944c
mtou <mtou@charougna.com>
2011-07-01 23:54:58 +0200

    Add new Domain model

---
+++ classes/chacms/core/model/domain.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Declares ChaCMS_Core_Model_Domain model
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS.Domain
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/model/domain.php
+ * @since     2011-06-20
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Core_Model_Domain model
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS.Domain
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/model/domain.php
+ */
+class ChaCMS_Core_Model_Domain extends ChaCMS_Model_Jelly
+{
+
+  /**
+   * Fields declaration
+   *
+   * @param Jelly_Meta $meta Access to Jelly framework
+   *
+   * @return null
+   */
+  public static function initialize(Jelly_Meta $meta)
+  {
+    $meta->table(ChaCMS::db_table_prefix().'domains')
+         ->fields(
+             array(
+              'id' =>    new Jelly_Field_Primary,
+              'code' =>  new Jelly_Field_String(array(
+                            'filters' => array(
+                                array('trim'),
+                                array('UTF8::strtolower'),
+                            ),
+                            'label' => 'Domain code',
+                            'name' => 'Domain code',
+                            'rules' => array(
+                                array('max_length', array(':value', 64)),
+                                array('mb_check_encoding', array(':value', 'UTF-8')),
+                            ),
+                            'unique' => TRUE,
+                          )),
+              'name' =>  new Jelly_Field_String(array(
+                            'filters' => array(
+                                array('trim'),
+                            ),
+                            'label' => 'Domain name',
+                            'name' => 'Domain name',
+                            'rules' => array(
+                                array('max_length', array(':value', 128)),
+                                array('mb_check_encoding', array(':value', 'UTF-8')),
+                                array('not_empty'),
+                            ),
+                          )),
+             )
+         );
+  }
+
+} // End class ChaCMS_Core_Model_Domain
---
+++ classes/model/chacms/domain.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Domain model
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS.Domain
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/model/chacms/domain.php
+ * @since     2011-06-20
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Domain model
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS.Domain
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/model/chacms/domain.php
+ */
+class Model_ChaCMS_Domain extends ChaCMS_Core_Model_Domain
+{
+
+} // End class Model_ChaCMS_Domain
