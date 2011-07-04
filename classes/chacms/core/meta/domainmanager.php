7efa15791ba7a389a6fc6aedfe37218a8a9b84b4
mtou <mtou@charougna.com>
2011-07-04 21:12:34 +0200

    Add Domain Manager

---
+++ classes/chacms/core/meta/domainmanager.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Declares ChaCMS_Core_Meta_DomainManager
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/meta/domainmanager.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Core_Meta_DomainManager
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/core/meta/domainmanager.php
+ */
+abstract class ChaCMS_Core_Meta_DomainManager
+{
+  /**
+   * Creates this instance
+   *
+   * @return null
+   */
+  public function __construct()
+  {
+
+  }
+
+} // End class ChaCMS_Core_Meta_DomainManager
---
+++ classes/chacms/meta/domainmanager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Meta_DomainManager
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/meta/domainmanager.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Meta_DomainManager
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/meta/domainmanager.php
+ */
+class ChaCMS_Meta_DomainManager extends ChaCMS_Core_Meta_DomainManager
+{
+
+} // End class ChaCMS_Meta_DomainManager
