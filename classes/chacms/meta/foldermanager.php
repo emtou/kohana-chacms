073fba360e3cefa310cb2a2cfae97a65dbb1c5f9
mtou <mtou@charougna.com>
2011-07-11 07:26:19 +0200

    Add Folder Manager

---
+++ classes/chacms/meta/foldermanager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares ChaCMS_Meta_FolderManager
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/meta/foldermanager.php
+ * @since     2011-06-30
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides ChaCMS_Meta_FolderManager
+ *
+ * PHP version 5
+ *
+ * @group ChaCMS
+ *
+ * @category  ChaCMS
+ * @package   ChaCMS
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-chacms/tree/master/classes/chacms/meta/foldermanager.php
+ */
+class ChaCMS_Meta_FolderManager extends ChaCMS_Core_Meta_FolderManager
+{
+
+} // End class ChaCMS_Meta_FolderManager
